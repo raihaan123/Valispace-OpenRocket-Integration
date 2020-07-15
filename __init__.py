b290b1fc246997cd13bd75cbfdc077c7f7466e61
raihaan123 <raihaan.usman@gmail.com>
2020-07-15 12:52:32 +0100

    update

--- __init__.py
+++ __init__.py
@@ -6,12 +6,20 @@ Created on Wed Jul 15 12:02:40 2020
 """
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-import routes
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
 
+@app.route('/auth', methods=['POST'])
+def login():
+    username = request.form['uname']
+    password = request.form['psw']
+    return render_template('login.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

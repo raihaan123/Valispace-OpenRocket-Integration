1647caf64cae024f761ee6b858923182c280943a
raihaan123 <raihaan.usman@gmail.com>
2020-07-15 13:07:43 +0100

    jdfhDKS

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('login.html')
+    return render_template("login.html")
 
 @app.route('/auth', methods=['POST'])
 def login():

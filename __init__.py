e840bf6cb971715d1d49185679a90236ae3cafa1
raihaan123 <raihaan.usman@gmail.com>
2020-07-15 13:45:12 +0100

    couldbeee

--- __init__.py
+++ __init__.py
@@ -6,22 +6,27 @@ Created on Wed Jul 15 12:02:40 2020
 """
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/login')
+@app.route('/home')
 def index():
     return render_template("success.html")
 
-@app.route('/auth', methods=['GET','POST'])
+@app.route('/sign_in', methods=['GET','POST'])
 def login():
-    username = request.form['uname']
-    password = request.form['psw']
+    if request.method == "POST":
+        
+        username = request.form['uname']
+        password = request.form['psw']
+        
+        return render_template("success.html")
+    
     return render_template("login.html")
-
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
